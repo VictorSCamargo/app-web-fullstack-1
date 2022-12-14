96d451218be10ee56537d0d5b4d7ffc4414102d6
Pedro Pereira <pmatiucci@gmail.com>
2022-12-14 17:42:32 -0300

    tela alterar senha  rota: /alterar

--- login/src/routes/index.jsx
+++ login/src/routes/index.jsx
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Login } from '../pages/Login'
 import { Register } from '../pages/Register'
+import { Alterar } from '../pages/Alterar'
 
 export const AppRouter = () => {
     return (
@@ -8,6 +9,7 @@ export const AppRouter = () => {
             <Routes>
                 <Route path='/login' exact element={<Login />} />
                 <Route path='/register' exact element={<Register />} />
+                <Route path='/alterar' exact element={<Alterar />} />
             </Routes>
         </Router>
     )

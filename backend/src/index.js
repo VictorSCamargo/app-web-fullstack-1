1760244b3abe8d9ac2a9d830dbd0aac2539d2320
pedromandelli <pbalconimandelli@gmail.com>
2022-12-15 10:20:39 -0300

    update integração, não funcional ainda

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,4 +3,4 @@ const Loaders = require('./loaders/index.js')
 
 Loaders.start();
 
-app.listen(3333, ()=> console.log('server is running'))
+app.listen(3333, ()=> console.log('server is running on port 3333'))

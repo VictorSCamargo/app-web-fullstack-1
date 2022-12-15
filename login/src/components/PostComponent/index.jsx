05fa46d475dd70ad9247cf6af8cf859f00d222f2
Pedro Pereira <pmatiucci@gmail.com>
2022-12-15 01:32:02 -0300

    Pagina dos posts apenas visual

---
+++ login/src/components/PostComponent/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css';
+
+export const PostComponent = (props) => {
+    return (
+        <div className="container">
+                <h1 className='username-text'>usuario</h1>
+                <text className='post-text'>review</text>
+            </div>
+    )
+}
--- login/src/routes/index.jsx
+++ login/src/routes/index.jsx
@@ -2,6 +2,8 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import { Login } from '../pages/Login'
 import { Register } from '../pages/Register'
 import { Alterar } from '../pages/Alterar'
+import { Posts } from '../pages/Posts'
+
 
 export const AppRouter = () => {
     return (
@@ -10,6 +12,7 @@ export const AppRouter = () => {
                 <Route path='/login' exact element={<Login />} />
                 <Route path='/register' exact element={<Register />} />
                 <Route path='/alterar' exact element={<Alterar />} />
+                <Route path='/post' exact element={<Posts />} />
             </Routes>
         </Router>
     )

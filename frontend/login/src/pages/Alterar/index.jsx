731dfc5282eb5c3fe9852de3b7087253e1f6bc24
Victor S. Camargo <victor.s.camargo@gmail.com>
2022-12-16 16:44:48 -0300

    Metodo fetch para alterar senha

--- frontend/login/src/pages/Alterar/index.jsx
+++ frontend/login/src/pages/Alterar/index.jsx
@@ -1,14 +1,62 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { LayoutComponents } from "../../components/LayoutComponents";
+
+const URL_ALTERAR_SENHA = ""
 
 export const Alterar = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [confirmpassword, setConfirmpassword] = useState("");
 
+  const navigate = useNavigate()
+
   async function requisitarAlteracaoDeSenha(event) {
     event.preventDefault();
+
+    if(password !== confirmpassword) {
+      alert("Senhas nao batem")
+    }
+    else{
+
+      const data_to_send = {
+          "username": username,
+          "password": password
+      };
+
+      const req = {
+          method: "POST",
+          mode: 'cors',
+          cache: "default",
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data_to_send)
+      };
+
+      try {
+        const res = await fetch(URL_ALTERAR_SENHA, req);
+        
+        const dados = await res.json()
+        
+        console.log(dados)
+
+        if(res.status !== 200) {
+          alert(dados.message)
+        }
+        else {
+          alert("Senha alterada com sucesso")
+
+          setTimeout(() => {
+            // 👇 Redirects to about page, note the `replace: true`
+            navigate('/login', { replace: true });
+          }, 10);
+        }
+      }
+      catch(e){
+        console.log("Falha ao comunicar com servidor")
+      }
+    }
   }
 
   return (

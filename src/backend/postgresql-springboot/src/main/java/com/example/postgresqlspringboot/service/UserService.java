62ea43e536be28c8217637c491b564c7f9cbcaf6
Victor S. Camargo <victor.s.camargo@gmail.com>
2022-12-27 19:49:55 -0300

    CRUD basico criado e testado
    Testado com Postman

---
+++ src/backend/postgresql-springboot/src/main/java/com/example/postgresqlspringboot/service/UserService.java
@@ -0,0 +1,48 @@
+package com.example.postgresqlspringboot.service;
+
+import com.example.postgresqlspringboot.entity.User;
+import com.example.postgresqlspringboot.entity.dto.UserDTO;
+import com.example.postgresqlspringboot.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import java.awt.print.Book;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<User> findALl() { return userRepository.findAll(); }
+
+    public User findById(Long id) {
+        Optional<User> entity = userRepository.findById(id);
+        if (entity.isPresent()){
+            return entity.get();
+        }
+        throw new RuntimeException();
+    }
+
+    public User save(UserDTO dto) {
+        User entity = new User();
+        entity.setUsername(dto.getUsername());
+        entity.setPassword(dto.getPassword());
+
+        return userRepository.save(entity);
+    }
+
+    public User update(User entity, UserDTO dto) {
+        entity.setUsername(dto.getUsername());
+        entity.setPassword(dto.getPassword());
+
+        return userRepository.save(entity);
+    }
+
+    public void delete(Long id) {
+        userRepository.deleteById(id);
+    }
+}
